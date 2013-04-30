f7e073c498d909a213229b8130fdadf284411bfc
Matt Simerson <matt@tnpi.net>
2013-04-30 12:38:16 -0700

    Policy: added setter validation and tests

--- lib/Mail/DMARC/Policy.pm
+++ lib/Mail/DMARC/Policy.pm
@@ -2,94 +2,372 @@ package Mail::DMARC::Policy;
 use strict;
 use warnings;
 
+=head1 NAME
+
+Mail::DMARC::Policy
+
+=head1 SYNOPSIS
+
+A DMARC policy as an object
+
+=head1 USAGE
+
+    my $pol = Mail::DMARC::Policy->new(
+            'v=DMARC1; p=none; rua=mailto:dmarc@example.com'
+            );
+
+    if ( $pol->v ne 'DMARC1' ) {
+        print "not a valid DMARC version!"
+    };
+
+    if ( $pol->p eq 'none' ) {
+        print "do nothing";
+    };
+
+=head1 METHODS
+
+=cut
+
+my %defaults = (
+    adkim => 'r',
+    aspf  => 'r',
+    fo    => 0,
+    ri    => 86400,
+    rf    => 'afrf',
+    pct   => 100,
+        );
+
 sub new {
+
+=head2 new
+
+An empty policy:
+
+   my $pol = Mail::DMARC::Policy->new;
+
+A policy from named arguments:
+
+   my $pol = Mail::DMARC::Policy->new(
+            v   => 'DMARC1',
+            p   => 'none',
+            pct => 50,
+           );
+
+A new policy from a DNS format DMARC record:
+
+   my $pol = Mail::DMARC::Policy->new(
+            'v=DMARC1; p=reject; rua=mailto:dmarc@example.com; ruf=mailto:dmarc@example.com;'
+           );
+
+If a policy is passed in (in either format), the resulting policy object will be populated with default values for any unspecified parameters. For example, p and v are required and thus have no 'default' values. The alignment specifiers adkim and aspf default to 'r' (relaxed) and will be populated if not present in the request.
+
+=cut
+
     my $class = shift;
-    my $self = {
-        p     => 'none',
-        adkim => 'r',
-        aspf  => 'r',
-        pct   => 100,
-    };
-    my $pack = ref $class ? ref $class : $class;
-    bless $self, $pack;
-    if ( 1 == @_ ) {   # a string to parse
-        return $self->parse( shift );
-    };
+    my $self = bless {}, $class;
+    my $package = ref $class ? ref $class : $class;
+    bless $self, $package;
+
+    return $self                 if 0 == @_;  # no arguments
+    return $self->parse( shift ) if 1 == @_;  # a string to parse
+
     die "invalid arguments" if @_ && @_ % 2 != 0;
-    return bless { %$self, @_ }, $pack;  # @_ values will override defaults 
+    my $policy = { %defaults, @_ };
+    bless $policy, $package;
+    die "invalid  policy" unless $self->is_valid( $policy );
+    return bless $policy, $package;  # @_ values will override defaults
 };
 
-# v=DMARC1;    (version)
+sub parse {
+
+=head2 parse
+
+Accepts a string containing a DMARC Resource Record, as it would be retrieved
+via DNS.
+
+    my $pol = Mail::DMARC::Policy->new;
+    $pol->parse( 'v=DMARC1; p=none; rua=mailto:dmarc@example.com' );
+
+=cut
+
+
+    my $self = shift;
+    die "invalid parse request" if 1 != scalar @_;
+    my $str = shift;
+
+    $str =~ s/\s//g;                         # remove all whitespace
+    $str =~ s/\\;/;/;                        # replace \; with ;
+    my %dmarc = map { split /=/, $_ } split /;/, $str;
+    my $policy = { %defaults, %dmarc };
+    die "invalid policy" if ! $self->is_valid( $policy );
+    return bless $policy, ref $self;  # inherited defaults + overrides
+}
+
+=head1 Record Format
+
+The following descriptions of each DMARC record tag and its corresponding values is copy/pasted from the March 31, 2013 draft:
+
+  https://datatracker.ietf.org/doc/draft-kucherawy-dmarc-base/?include_text=1
+
+=cut
+
 sub v {
     return $_[0]->{v} if 1 == scalar @_;
-    $_[0]->{v} = $_[1];
+    die "invalid DMARC version" if 'DMARC1' ne $_[1];
+    return $_[0]->{v} = $_[1];
+
+=head2 v
+
+Version (plain-text; REQUIRED).  Identifies the record retrieved
+as a DMARC record.  It MUST have the value of "DMARC1".  The value
+of this tag MUST match precisely; if it does not or it is absent,
+the entire retrieved record MUST be ignored.  It MUST be the first
+tag in the list.
+
+=cut
+
 };
 
-# p=none;      (disposition policy : reject, quarantine, none (monitor))
 sub p {
     return $_[0]->{p} if 1 == scalar @_;
-    $_[0]->{p} = $_[1];
+    die "invalid p" unless $_[0]->is_valid_p($_[1]);
+    return $_[0]->{p} = $_[1];
+
+=head2 p
+
+Requested Mail Receiver policy (plain-text; REQUIRED for policy
+records).  Indicates the policy to be enacted by the Receiver at
+the request of the Domain Owner.  Policy applies to the domain
+queried and to sub-domains unless sub-domain policy is explicitly
+described using the "sp" tag.  This tag is mandatory for policy
+records only, but not for third-party reporting records (see
+Section 8.2).
+
+=cut
+
 };
 
 # sp=reject;   (subdomain policy: default, same as p)
 sub sp {
     return $_[0]->{sp} if 1 == scalar @_;
-    $_[0]->{sp} = $_[1];
+    die "invalid sp" unless $_[0]->is_valid_p($_[1]);
+    return $_[0]->{sp} = $_[1];
+
+=head2 sp
+
+{R6} Requested Mail Receiver policy for subdomains (plain-text;
+OPTIONAL).  Indicates the policy to be enacted by the Receiver at
+the request of the Domain Owner.  It applies only to subdomains of
+the domain queried and not to the domain itself.  Its syntax is
+identical to that of the "p" tag defined above.  If absent, the
+policy specified by the "p" tag MUST be applied for subdomains.
+
+=cut
+
 };
 
-# adkim=s;     (dkim alignment: s=strict, r=relaxed)
 sub adkim {
     return $_[0]->{adkim} if 1 == scalar @_;
-    $_[0]->{adkim} = $_[1];
+    die "invalid adkim" unless grep {/^$_[1]$/} qw/ r s /;
+    return $_[0]->{adkim} = $_[1];
+
+=head2 adkim
+
+(plain-text; OPTIONAL, default is "r".)  Indicates whether or
+not strict DKIM identifier alignment is required by the Domain
+Owner.  If and only if the value of the string is "s", strict mode
+is in use.  See Section 4.3.1 for details.
+
+=cut
+
 };
 
-# aspf=r;      (spf  alignment: s=strict, r=relaxed)
 sub aspf {
     return $_[0]->{aspf} if 1 == scalar @_;
-    $_[0]->{aspf} = $_[1];
+    die "invalid aspf" unless grep {/^$_[1]$/} qw/ r s /;
+    return $_[0]->{aspf} = $_[1];
+
+=head2 aspf
+
+(plain-text; OPTIONAL, default is "r".)  Indicates whether or
+not strict SPF identifier alignment is required by the Domain
+Owner.  If and only if the value of the string is "s", strict mode
+is in use.  See Section 4.3.2 for details.
+
+=cut
+
 };
 
-# rua=mailto: dmarc-feedback@example.com; (aggregate reports)
+sub fo {
+    return $_[0]->{fo} if 1 == scalar @_;
+    die "invalid fo" unless grep {/^$_[1]$/} qw/ 0 1 d s /;
+    return $_[0]->{fo} = $_[1];
+
+=head2 fo
+
+Failure reporting options (plain-text; OPTIONAL, default "0"))
+Provides requested options for generation of failure reports.
+Report generators MAY choose to adhere to the requested options.
+This tag's content MUST be ignored if a "ruf" tag (below) is not
+also specified.  The value of this tag is a colon-separated list
+of characters that indicate failure reporting options as follows:
+
+  0: Generate a DMARC failure report if all underlying
+     authentication mechanisms failed to produce an aligned "pass"
+     result.
+
+  1: Generate a DMARC failure report if any underlying
+     authentication mechanism failed to produce an aligned "pass"
+     result.
+
+  d: Generate a DKIM failure report if the message had a signature
+     that failed evaluation, regardless of its alignment.  DKIM-
+     specific reporting is described in [AFRF-DKIM].
+
+  s: Generate an SPF failure report if the message failed SPF
+     evaluation, regardless of its alignment. SPF-specific
+     reporting is described in [AFRF-SPF].
+
+=cut
+
+};
+
 sub rua {
     return $_[0]->{rua} if 1 == scalar @_;
-    $_[0]->{rua} = $_[1];
+    return $_[0]->{rua} = $_[1];
+
+#TODO: validate as comma spaced list of URIs
+
+=head2 rua
+
+Addresses to which aggregate feedback is to be sent (comma-
+separated plain-text list of DMARC URIs; OPTIONAL). {R11} A comma
+or exclamation point that is part of such a DMARC URI MUST be
+encoded per Section 2.1 of [URI] so as to distinguish it from the
+list delimiter or an OPTIONAL size limit.  Section 8.2 discusses
+considerations that apply when the domain name of a URI differs
+from that of the domain advertising the policy.  See Section 15.6
+for additional considerations.  Any valid URI can be specified.  A
+Mail Receiver MUST implement support for a "mailto:" URI, i.e. the
+ability to send a DMARC report via electronic mail.  If not
+provided, Mail Receivers MUST NOT generate aggregate feedback
+reports.  URIs not supported by Mail Receivers MUST be ignored.
+The aggregate feedback report format is described in Section 8.3.
+
+=cut
+
 };
 
-# ruf=mailto: dmarc-feedback@example.com; (forensic reports)
+
 sub ruf {
     return $_[0]->{ruf} if 1 == scalar @_;
-    $_[0]->{ruf} = $_[1];
+    return $_[0]->{ruf} = $_[1];
+
+#TODO: validate as comma spaced list of URIs
+
+=head2 ruf
+
+Addresses to which message-specific failure information is to
+be reported (comma-separated plain-text list of DMARC URIs;
+OPTIONAL). {R11} If present, the Domain Owner is requesting Mail
+Receivers to send detailed failure reports about messages that
+fail the DMARC evaluation in specific ways (see the "fo" tag
+above).  The format of the message to be generated MUST follow
+that specified in the "rf" tag.  Section 8.2 discusses
+considerations that apply when the domain name of a URI differs
+from that of the domain advertising the policy.  A Mail Receiver
+MUST implement support for a "mailto:" URI, i.e. the ability to
+send a DMARC report via electronic mail.  If not provided, Mail
+Receivers MUST NOT generate failure reports.  See Section 15.6 for
+additional considerations.
+
+=cut
+
 };
 
-# rf=afrf;     (report format: afrf, iodef)
 sub rf {
     return $_[0]->{rf} if 1 == scalar @_;
-    $_[0]->{rf} = $_[1];
+    foreach my $f ( split /,/, $_[1] ) {
+        die "invalid format: $f" if ! $_[0]->is_valid_rf( lc $f );
+    }
+    return $_[0]->{rf} = $_[1];
+
+=head2 rf
+
+Format to be used for message-specific failure reports (comma-
+separated plain-text list of values; OPTIONAL; default "afrf").
+The value of this tag is a list of one or more report formats as
+requested by the Domain Owner to be used when a message fails both
+[SPF] and [DKIM] tests to report details of the individual
+failure.  The values MUST be present in the registry of reporting
+formats defined in Section 14; a Mail Receiver observing a
+different value SHOULD ignore it, or MAY ignore the entire DMARC
+record.  Initial default values are "afrf" (defined in [AFRF]) and
+"iodef" (defined in [IODEF]).  See Section 8.4 for details.
+
+=cut
+
 };
 
-# ri=8400;     (report interval)
 sub ri {
     return $_[0]->{ri} if 1 == scalar @_;
+    die unless ($_[1] eq int($_[1]) && $_[1] >= 0 && $_[1] <= 4294967295);
     $_[0]->{ri} = $_[1];
+
+=head2 ri
+
+Interval requested between aggregate reports (plain-text, 32-bit
+unsigned integer; OPTIONAL; default 86400). {R14} Indicates a
+request to Receivers to generate aggregate reports separated by no
+more than the requested number of seconds.  DMARC implementations
+MUST be able to provide daily reports and SHOULD be able to
+provide hourly reports when requested.  However, anything other
+than a daily report is understood to be accommodated on a best-
+effort basis.
+
+=cut
+
 };
 
-# pct=50;      (percent of messages to filter)
 sub pct {
     return $_[0]->{pct} if 1 == scalar @_;
-    $_[0]->{pct} = $_[1];
+    die unless ($_[1] eq int($_[1]) && $_[1] >= 0 && $_[1] <= 100 );
+    return $_[0]->{pct} = $_[1];
+
+=head2 pct
+
+(plain-text integer between 0 and 100, inclusive; OPTIONAL;
+default is 100). {R8} Percentage of messages from the DNS domain's
+mail stream to which the DMARC mechanism is to be applied.
+However, this MUST NOT be applied to the DMARC-generated reports,
+all of which must be sent and received unhindered.  The purpose of
+the "pct" tag is to allow Domain Owners to enact a slow rollout
+enforcement of the DMARC mechanism.  The prospect of "all or
+nothing" is recognized as preventing many organizations from
+experimenting with strong authentication-based mechanisms.  See
+Section 7.1 for details.
+
+=cut
+
+};
+
+sub is_valid_rf {
+    my ($self, $f) = @_;
+    return (grep {/^$f$/} qw/ iodef rfrf /) ? 1 : 0;
+};
+
+sub is_valid_p {
+    my ($self, $p) = @_;
+    return (grep {/^$p$/} qw/ none reject quarantine /) ? 1 : 0;
 };
 
 sub is_valid {
-    my %valid = map { $_ => 1 } qw/ none reject quarantine /;
-    return $valid{$_[1]} ? 1 : 0;
+    my ($self, $obj) = @_;
+    die "missing version specifier" if ! $obj->{v};
+    die "invalid version" if 'dmarc1' ne lc $obj->{v};
+    die "missing policy action" if ! $obj->{p};
+    die "invalid policy action" if ! $self->is_valid_p( $obj->{p} );
+    return 1;
 };
-
-sub parse {
-    my ($self, $str) = @_;
-    $str =~ s/\s//g;                             # remove all whitespace
-    my %dmarc = map { split /=/, $_ } split /;/, $str;
-    return bless { %$self, %dmarc }, ref $self;  # inherited defaults + overrides
-#return Mail::DMARC::Policy->new( %dmarc );
-}
 
 1;
--- t/Policy.t
+++ t/Policy.t
@@ -16,37 +16,88 @@ ok( $pol->v('DMARC1'), "policy, set");
 cmp_ok( $pol->v, 'eq', 'DMARC1', "policy, version, pos" );
 
 test_new();
-test_is_valid();
+test_is_valid_p();
 test_parse();
+test_setters_valid();
 
 done_testing();
 exit;
 
+sub test_setters_valid {
+    my %good_vals = (
+            p      => [ qw/ none reject quarantine / ],
+            v      => [ 'DMARC1' ],
+            sp     => [ qw/ none reject quarantine / ],
+            adkim  => [ qw/ r s / ],
+            aspf   => [ qw/ r s / ],
+            fo     => [ qw/ 0 1 d s / ],
+#TODO       rua    =>
+#TODO       ruf    =>
+            rf     => [ qw/ iodef rfrf / ],
+            ri     => [ 0, 1, 1000, 4294967295 ],
+            pct    => [ 0, 10, 50, 99, 100 ],
+            );
+
+    foreach my $k ( keys %good_vals ) {
+        foreach my $t ( @{$good_vals{$k}} ) {
+            ok( defined $pol->$k( $t ), "$k, $t");
+        };
+    };
+
+    my %bad_vals = (
+            p      => [ qw/ nonense silly example / ],
+            v      => [ 'DMARC2' ],
+            sp     => [ qw/ nones rejection quarrantine / ],
+            adkim  => [ qw/ relaxed strict / ],
+            aspf   => [ qw/ relaxed strict / ],
+            fo     => [ qw/ 00 11 dd ss / ],
+#TODO       rua    =>
+#TODO       ruf    =>
+            rf     => [ qw/ iodef2 rfrf2 / ],
+            ri     => [ -1, 'a', 4294967296 ],
+            pct    => [ -1, 'f', 101 ],
+            );
+
+    foreach my $k ( keys %bad_vals ) {
+        foreach my $t ( @{$bad_vals{$k}} ) {
+            eval { $pol->$k( $t ); };
+            ok( $@, "neg, $k, $t");
+        };
+    };
+};
+
 sub test_new {
-# default policy
+# empty policy
     my $pol = Mail::DMARC::Policy->new();
     isa_ok( $pol, 'Mail::DMARC::Policy' );
-    is_deeply( $pol, { p => 'none', pct=>100, adkim=>'r', aspf=>'r' }, "new" );
+    is_deeply( $pol, {}, "new, empty policy" );
 
 # default policy
-    $pol = Mail::DMARC::Policy->new( p=>'reject',pct => 90, rua=>'mailto:u@d.co' );
+    $pol = Mail::DMARC::Policy->new( v=>'DMARC1', p=>'reject',pct => 90, rua=>'mailto:u@d.co' );
     isa_ok( $pol, 'Mail::DMARC::Policy' );
-    is_deeply( $pol, { p => 'reject', pct=>90, adkim=>'r', aspf=>'r', rua=>'mailto:u@d.co' }, "new, with args" );
+    is_deeply( $pol, { v=>'DMARC1', p => 'reject', pct=>90, adkim=>'r', aspf=>'r', ri=>86400, fo=>0, rf=>'afrf', rua=>'mailto:u@d.co' }, "new, with args" );
+
+# text record
+    $pol = Mail::DMARC::Policy->new( 'v=DMARC1; p=reject; rua=mailto:u@d.co; pct=90' );
+    isa_ok( $pol, 'Mail::DMARC::Policy' );
+    is_deeply( $pol, { v=>'DMARC1', p => 'reject', pct=>90, adkim=>'r', aspf=>'r', ri=>86400, fo=>0, rf=>'afrf', rua=>'mailto:u@d.co' }, "new, with args" );
 };
 
 sub test_parse {
-    $pol = $pol->parse( 'v=DMARC1; p=reject; rua=mailto:dmarc@example.com; ruf=mailto:dmarc@example.com; pct=90', 'parse');
+
+    $pol = $pol->parse( 'v=DMARC1; p=reject; rua=mailto:dmarc@example.co; pct=90');
     isa_ok( $pol, 'Mail::DMARC::Policy' );
-    is_deeply( $pol, { v=>'DMARC1', p => 'reject', pct=>90, adkim=>'r', aspf=>'r', rua=>'mailto:dmarc@example.com', ruf=>'mailto:dmarc@example.com', }, "parse" );
+    is_deeply( $pol, { v=>'DMARC1', p => 'reject', pct=>90, adkim=>'r', aspf=>'r', fo=>0, ri=>86400, rf=>'afrf', rua=>'mailto:dmarc@example.co', }, 'parse' );
+
 };
 
-sub test_is_valid{
+sub test_is_valid_p {
     foreach my $p ( qw/ none reject quarantine / ) {
-        ok( $pol->is_valid( $p ), "policy->is_valid, pos, $p" );
+        ok( $pol->is_valid_p ( $p ), "policy->is_valid_p, pos, $p" );
     };
 
     foreach my $p ( qw/ other gibberish non-policy words / ) {
-        ok( ! $pol->is_valid( $p ), "policy->is_valid, neg, $p" );
+        ok( ! $pol->is_valid_p ( $p ), "policy->is_valid_p, neg, $p" );
     };
 };
 
