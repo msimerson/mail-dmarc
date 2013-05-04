817130f04e75cd415c5ecc3aeb353d2dfbff1113
Matt Simerson <matt@tnpi.net>
2013-05-03 23:12:03 -0700

    added accessors for Mail::DMARC object

--- lib/Mail/DMARC.pm
+++ lib/Mail/DMARC.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Carp;
+use Data::Dumper;
+use Regexp::Common qw /net/;
 
 =head1 SYNOPSIS
 
@@ -11,29 +13,74 @@ DMARC: a reliable means to authenticate who mail is from.
 
 =cut
 
+sub new {
+    my ($class, @args) = @_;
+    croak "invalid arguments" if @args % 2 != 0;
+    return bless { @args }, $class;
+};
+
+sub source_ip {
+    return $_[0]->{source_ip} if 1 == scalar @_;
+    croak "invalid source_ip" if ! $_[0]->is_valid_ip($_[1]);
+    return $_[0]->{source_ip} = $_[1];
+};
+
+sub envelope_to {
+    return $_[0]->{envelope_to} if 1 == scalar @_;
+    croak "invalid envelope_to" if ! $_[0]->is_valid_domain($_[1]);
+    return $_[0]->{envelope_to} = $_[1];
+};
+
+sub envelope_from {
+    return $_[0]->{envelope_from} if 1 == scalar @_;
+    croak "invalid envelope_from" if ! $_[0]->is_valid_domain($_[1]);
+    return $_[0]->{envelope_from} = $_[1];
+};
+
+sub header_from {
+    return $_[0]->{header_from} if 1 == scalar @_;
+    croak "invalid header_from" if ! $_[0]->is_valid_domain($_[1]);
+    return $_[0]->{header_from} = $_[1];
+};
+
+sub dkim {
+    my ($self, $dkim) = @_;
+
+    if ( ref $dkim && ref $dkim eq 'Mail::DKIM::Verifier' ) {
+# A DKIM verifier will have result and signature methods.
+        foreach my $s ( $dkim->signatures ) {
+            push @{ $self->{dkim} }, {
+                domain       => $s->domain,
+                selector     => $s->selector,
+                result       => $s->result,
+                human_result => $s->result_detail,
+            };
+        };
+        return $self->{dkim};
+    };
+
+    if ( 'ARRAY' ne ref $dkim ) {
+        croak "dkim must be an array reference!";
+    };
+    return $self->{dkim} = $dkim;
+};
+
+sub spf {
+    my ($self, @args) = @_;
+
+    if ( scalar @args == 1 && ref $args[0] && ref $args[0] eq 'HASH' ) {
+        return $self->{spf} = $args[0];
+    };
+
+    croak "invalid arguments" if @args % 2 != 0;
+    return $self->{spf} = { @args };
+};
+
 sub inputs {
     my ($self, $dkim, $spf) = @_;
-    return
-    {
-        source_ip       => '',
-        envelope_to     => '',   # envelope recipient domain
-        envelope_from   => '',   # envelope from domain
-        header_from     => '',   # header from domain
-        header_from_raw => '',   # in lieu of header_from
-
-        dkim => [
-            {
-                domain      => $dkim->signature->domain,   # The d= parameter in the signature
-                selector    => $dkim->signature->selector, # The s= parameter in the signature
-                result      => $dkim->result,  # none,pass,fail,policy,neutral,temperror, permerror
-                human_result=> $dkim->result_detail,
-            },
-        ],
-        spf => {
-            domain => $spf->identity, # checked domain
-            scope  => $spf->scope, # scope of checked domain: mfrom, helo
-            result => $spf->code,  # none, neutral, pass, fail, softfail, temperror, permerror
-        },
+    return {
+        report_domain => 'great.co',
+        report_org    => 'My Great Company',
     }
 };
 
@@ -59,4 +106,166 @@ sub result {
     };
 };
 
+sub is_valid_ip {
+    my ($self, $ip) = @_;
+    if ( $ip =~ /:/ ) {
+        return 1 if $ip =~ /^$RE{net}{IPv6}$/;
+        return 0;
+    };
+
+    return 1 if $ip =~ /^$RE{net}{IPv4}$/;
+    return 0;
+};
+
+sub is_valid_domain {
+    my ($self, $domain) = @_;
+    return 1 if $domain =~ /^$RE{net}{domain}$/;
+    return 0;
+};
+
 1;
+
+__END__
+
+sub {};  # for vim automatic code folding
+
+=head1 DESCRIPTION
+
+Determine if:
+
+    a. the header_from domain exists
+    b. the header_from domain publishes a DMARC policy
+    c. if a policy is found, does the message conform?
+
+=head1 METHODS
+
+=head2 new
+
+Create a new empty DMARC object. Then populate it and run the request:
+
+    my $dmarc = Mail::DMARC->new;
+    $dmarc->source_ip('192.0.1.1');
+    $dmarc->envelope_to('recipient.example.com');
+    $dmarc->envelope_from('sender.example.com');
+    $dmarc->header_from('sender.example.com');
+    $dmarc->dkim( $dkim_verifier );
+    $dmarc->spf(
+        domain => 'example.com',
+        scope  => 'mfrom',
+        result => 'pass',
+            );
+    my $result = $dmarc->verify();
+
+Alternatively, you can pass in all the required parameters in one shot:
+
+    my $dmarc = Mail::DMARC->new(
+            source_ip     => '192.0.1.1',
+            envelope_to   => 'example.com',
+            envelope_from => 'cars4you.info',
+            header_from   => 'yahoo.com',
+            dkim          => $dkim_results,
+            spf           => $spf_results,
+            );
+    my $result = $dmarc->verify();
+
+=head2 source_ip
+
+The remote IP that attempted sending the message. DMARC only uses this data for reporting to domains that request DMARC reports.
+
+=head2 envelope_to
+
+The domain portion of the RFC5321.RcptTo, (aka, the envelope recipient), and the bold portion in the following example:
+
+=over 8
+
+RCPT TO:<user@B<example.com>>
+
+=back
+
+=head2 envelope_from
+
+The domain portion of the RFC5321.MailFrom, (aka, the envelope sender). That is the the bold portion in the following example:
+
+=over 8
+
+MAIL FROM:<user@B<example.com>>
+
+=back
+
+=head2 header_from
+
+The domain portion of the RFC5322.From, aka, the From message header.
+
+=over 8
+
+From: Ultimate Vacation <sweepstakes@B<example.com>>
+
+=back
+
+You can instead pass in the entire From: header with header_from_raw.
+
+=head2 header_from_raw
+
+This retrieves the header_from domain by extracing it from a raw From field/header.  The domain portion is extracted by Mail::DMARC::PurePerl::get_dom_from_header, which is fast, generally effective, but also rather crude. It does have limits, so read the description.
+
+=head2 dkim
+
+The dkim method accepts an array reference. Each array element represents a DKIM signature in the message and consists of the 4 keys shown in this example:
+
+    $dmarc->dkim( [
+            {
+                domain      => 'example.com',
+                selector    => 'apr2013',
+                result      => 'fail',
+                human_result=> 'fail (body has been altered)',
+            },
+            {
+                # 2nd signature, if present
+            },
+        ] );
+
+If you used Mail::DKIM::Verifier to validate the message, just pass in the Mail::DKIM::Verifier object that processed the message:
+
+    $dmarc->dkim( $dkim_verifier );
+
+=head3 domain
+
+The d= parameter in the signature
+
+=head3 selector
+
+The s= parameter in the signature
+
+=head3 result
+
+The validation results of this signature. One of: none, pass, fail, policy, neutral, temperror, or permerror
+
+=head3 human result
+
+Additional information about the DKIM result. This is comparable to Mail::DKIM::Verifier->result_detail.
+
+=head2 spf
+
+The spf method accepts a hashref or named arguments:
+
+    $dmarc->spf(
+        domain => 'example.com',
+        scope  => 'mfrom',
+        result => 'pass',
+    );
+
+The SPF domain and result are required for DMARC validation and the scope is used for reporting.
+
+=head3 domain
+
+The SPF checked domain
+
+=head3 scope
+
+The scope of the checked domain: mfrom, helo
+
+=head3 result
+
+The SPF result code: none, neutral, pass, fail, softfail, temperror, or permerror.
+
+=cut
---
+++ t/00.Dmarc.t
@@ -0,0 +1,101 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+use lib 'lib';
+
+use_ok( 'Mail::DMARC' );
+
+my $dmarc = Mail::DMARC->new();
+isa_ok( $dmarc, 'Mail::DMARC' );
+
+my %sample_dmarc = (
+        source_ip     => '192.0.1.1',
+        envelope_to   => 'example.com',
+        envelope_from => 'cars4you.info',
+        header_from   => 'yahoo.com',
+        dkim          => [ {
+                domain      => 'example.com',
+                selector    => 'apr2013',
+                result      => 'fail',
+                human_result=> 'fail (body has been altered)',
+        } ],
+        spf           => {
+                domain => 'example.com',
+                scope  => 'mfrom',
+                result => 'pass',
+        },
+    );
+
+test_new();
+test_is_valid_ip();
+test_is_valid_domain();
+test_setter_values();
+
+done_testing();
+exit;
+
+sub test_setter_values {
+    my %good_vals = (
+            source_ip     => [ qw/ 0.0.0.0 1.1.1.1 255.255.255.255 / ],
+            envelope_to   => [ qw/ example.com / ],
+            envelope_from => [ qw/ example.com / ],
+            header_from   => [ qw/ spam-example.com / ],
+            dkim          => [ $sample_dmarc{dkim} ],
+            spf           => [ $sample_dmarc{spf} ],
+            );
+
+    foreach my $k ( keys %good_vals ) {
+        foreach my $t ( @{$good_vals{$k}} ) {
+            ok( defined $dmarc->$k( $t ), "$k, $t");
+        };
+    };
+
+    my %bad_vals = (
+            source_ip     => [ qw/ 0.257.0.25 255.255.255.256 / ],
+            envelope_to   => [ qw/ 1.a / ],
+            envelope_from => [ qw/ / ],
+            header_from   => [ qw/ / ],
+            dkim          => [ qw/ / ],
+            spf           => [ qw/ / ],
+            );
+
+    foreach my $k ( keys %bad_vals ) {
+        foreach my $t ( @{$bad_vals{$k}} ) {
+            eval { $dmarc->$k( $t ); };
+            ok( $@, "neg, $k, $t");
+        };
+    };
+};
+
+
+sub test_new {
+# empty policy
+    my $dmarc = Mail::DMARC->new();
+    isa_ok( $dmarc, 'Mail::DMARC' );
+    is_deeply( $dmarc, {}, "new, empty" );
+
+# new, one shot request
+    $dmarc = Mail::DMARC->new( %sample_dmarc );
+    isa_ok( $dmarc, 'Mail::DMARC' );
+    is_deeply( $dmarc, \%sample_dmarc, "new, one shot" );
+
+# new, individual accessors
+    $dmarc = Mail::DMARC->new();
+    isa_ok( $dmarc, 'Mail::DMARC' );
+    foreach my $key ( keys %sample_dmarc ) {
+        $dmarc->$key( $sample_dmarc{$key} );
+    };
+    is_deeply( $dmarc, \%sample_dmarc, "new, individual accessors" );
+};
+
+sub test_is_valid_ip {
+    ok( $dmarc->is_valid_ip('1.1.1.1'), "is_valid_ip");
+};
+
+sub test_is_valid_domain {
+
+    ok( $dmarc->is_valid_domain('example.com'), 'is_valid_domain');
+};
+
