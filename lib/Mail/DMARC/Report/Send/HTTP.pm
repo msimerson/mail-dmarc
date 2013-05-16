30752fc1f3ed469e7a49d6cfe97e3491487987f7
Matt Simerson <matt@tnpi.net>
2013-05-16 09:58:31 -0700

    baby step towards HTTP report sending

--- lib/Mail/DMARC/Report/Send/HTTP.pm
+++ lib/Mail/DMARC/Report/Send/HTTP.pm
@@ -4,9 +4,26 @@ use warnings;
 
 use Carp;
 #use Data::Dumper;
+use Net::HTTP;
 
 use parent 'Mail::DMARC::Base';
 
+sub rua_post {
+    my ($self, $args) = @_;
+
+    my $ver = $Mail::DMARC::VERSION;
+    my $s = Net::HTTP->new(Host => $args->{host} ) or die $@;
+    $s->write_request(POST => "$args->{path}", 'User-Agent' => "Mail::DMARC/$ver");
+    my($code, $mess, %h) = $s->read_response_headers;
+
+    while (1) {
+        my $buf;
+        my $n = $s->read_entity_body($buf, 1024);
+        die "read failed: $!" unless defined $n;
+        last unless $n;
+        print $buf;
+    }
+};
 
 1;
 # ABSTRACT: send DMARC reports via HTTP
