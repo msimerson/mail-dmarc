9e42f96a36bba7a1cf812339e4bfcb3440c92090
Matt Simerson <matt@tnpi.net>
2019-12-23 15:17:49 -0800

    restore some verbose output when asked for

--- lib/Mail/DMARC/Report/Store/SQL.pm
+++ lib/Mail/DMARC/Report/Store/SQL.pm
@@ -141,11 +141,13 @@ sub delete_report {
 
     if (scalar @row_ids) {
         foreach my $table (qw/ report_record_spf report_record_dkim report_record_reason /) {
+            print "deleting $table rows " . join(',', @row_ids) . "\n" if $self->verbose;
             eval { $self->query( $self->grammar->delete_from_where_record_in($table, \@row_ids)); };
             # warn $@ if $@;
         }
     }
     foreach my $table (qw/ report_policy_published report_record report_error /) {
+        print "deleting $table rows for report $report_id\n" if $self->verbose;
         eval { $self->query( $self->grammar->delete_from_where_report( $table, [$report_id] )); };
         # warn $@ if $@;
     }
