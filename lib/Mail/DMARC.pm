529e9c3cd9bddceffc1b8370d7c0ac38e9b37568
Matt Simerson <matt@tnpi.net>
2026-02-26 19:06:22 -0800

    doc: bump copyright to 2026

--- lib/Mail/DMARC.pm
+++ lib/Mail/DMARC.pm
@@ -654,7 +654,7 @@ Ricardo Signes <rjbs@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2025 by Matt Simerson.
+This software is copyright (c) 2026 by Matt Simerson.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
--- lib/Mail/DMARC/Report/Aggregate/Record/Auth_Results.pm
+++ lib/Mail/DMARC/Report/Aggregate/Record/Auth_Results.pm
@@ -101,7 +101,7 @@ Marc Bradshaw <marc@marcbradshaw.net>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2025 by Matt Simerson.
+This software is copyright (c) 2026 by Matt Simerson.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
